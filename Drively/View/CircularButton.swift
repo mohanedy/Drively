9908c17577456e2885d6b6a208c9988b8ffb479a
Mohaned Yossry <memo.yossry@gmail.com>
2020-06-05 15:39:43 +0200

    Design Improvement

--- Drively/View/CircularButton.swift
+++ Drively/View/CircularButton.swift
@@ -9,48 +9,48 @@
 import UIKit
 @IBDesignable
 class CircularButton: UIButton {
-
-     var shadowOfSetWidth : CGFloat = 0
-        var shadowOfSetHeight : CGFloat = 5
-        
-        var shadowColor : UIColor = UIColor.gray
-        var shadowOpacity : CGFloat = 0.4
     
-       
-       @IBInspectable
-       var fillColor:CGColor = UIColor.black.cgColor
-       
-       override init(frame: CGRect) {
-           super.init(frame: frame)
-           sharedInit()
-       }
-       
-       required init?(coder aDecoder: NSCoder) {
-           super.init(coder: aDecoder)
-           sharedInit()
-       }
-       
-       override func prepareForInterfaceBuilder() {
-           sharedInit()
-       }
-       
+    var shadowOfSetWidth : CGFloat = 0
+    var shadowOfSetHeight : CGFloat = 5
+    
+    var shadowColor : UIColor = UIColor.gray
+    var shadowOpacity : CGFloat = 0.5
+    
+    
+    @IBInspectable
+    var fillColor:CGColor = UIColor.black.cgColor
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        sharedInit()
+    }
+    
     func sharedInit() {
         
         
         let cornerRadius = frame.width / 2
         layer.cornerRadius = cornerRadius
         imageView?.contentMode = .scaleAspectFill
-        layer.masksToBounds = true
-                 layer.shadowColor = shadowColor.cgColor
-                 
-                 layer.shadowOffset = CGSize(width: shadowOfSetWidth, height: shadowOfSetHeight)
-                 
-                 let shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
-                 
-                 layer.shadowPath = shadowPath.cgPath
-                 
-                 layer.shadowOpacity = Float(shadowOpacity)
-       }
+        
+        layer.shadowColor = shadowColor.cgColor
+        
+        layer.shadowOffset = CGSize(width: shadowOfSetWidth, height: shadowOfSetHeight)
+        
+        let shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
+        
+        layer.shadowPath = shadowPath.cgPath
+        
+        layer.shadowOpacity = Float(shadowOpacity)
+    }
     
-
+    
 }
